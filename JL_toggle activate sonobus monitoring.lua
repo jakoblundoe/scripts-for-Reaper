8051ff93ec69f6a7e615b0c00c32d53e0257311c
jlund <dev@jakoblundoe.com>
2024-10-10 11:39:54 +0200

    Add possibility to define target fx name in user config

--- JL_toggle activate sonobus monitoring.lua
+++ JL_toggle activate sonobus monitoring.lua
@@ -8,6 +8,10 @@
 
 -- USER CONFIG AREA -----------------------------------------------------------
 
+-- Define name of FX to toggle on/off.
+-- MUST be an exact match (except for upper and lower case letters).
+local targetFxName = "sonobus"
+
 ------------------------------------------------------- END OF USER CONFIG AREA
 local mastTrack = reaper.GetMasterTrack(0)
 
@@ -15,9 +19,10 @@ local monFxCount = reaper.TrackFX_GetRecCount(mastTrack)
 local monOffset = (0x1000000)-1;
 
 for i = 0, monFxCount do
-    local monitorPlugin = "VST3: SonoBus (Sonosaurus) (18ch)"
+    local monitorPlugin = targetFxName
     local retval, individualFxName = reaper.TrackFX_GetFXName(mastTrack, monOffset+i)
     if retval then
+        individualFxName = individualFxName:lower();
         if individualFxName == monitorPlugin then
             local fxIsEnabled = reaper.TrackFX_GetEnabled(mastTrack, monOffset+i)
             if fxIsEnabled then
